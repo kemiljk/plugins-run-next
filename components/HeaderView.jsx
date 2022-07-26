08ec57205bd7d68f3ffa9b424318a037c737ca33
Karl <karl@kejk.tech>
2022-07-26 07:14:05 +0100

    fix: text alignment

--- components/HeaderView.jsx
+++ components/HeaderView.jsx
@@ -4,7 +4,7 @@ export default function HeaderView({ children }) {
   return (
     <header className="mx-auto w-full">
       <div className="flex w-full justify-center px-4 pt-12 sm:px-6 lg:px-8">
-        <h1 className="font-mono text-3xl font-bold text-neutral-900 dark:text-neutral-200">
+        <h1 className="font-mono text-3xl font-bold text-neutral-900 dark:text-neutral-200 text-left md:text-center">
           {children}
         </h1>
       </div>
--- components/SubheaderView.jsx
+++ components/SubheaderView.jsx
@@ -4,7 +4,7 @@ export default function SubheaderView({ children }) {
   return (
     <div className="mx-auto max-w-3xl">
       <div className="flex w-full justify-center py-6 px-4 sm:px-6 lg:px-8">
-        <h2 className="text-center text-xl text-neutral-700 dark:text-neutral-300">
+        <h2 className="text-left md:text-center text-xl text-neutral-700 dark:text-neutral-300">
           {children}
         </h2>
       </div>
