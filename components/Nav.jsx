1c926975feb0e55c11e971f690ea3211f7c05a76
Karl <karl@kejk.tech>
2022-08-07 15:34:11 +0100

    fix: maybe background padding

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
                   alt="Logo"
                 />
               </div>
-              <div className="block bg-neutral-200 rounded-xl">
+              <div className="block bg-neutral-200 rounded-xl py-4 px-3">
                 <div className="flex items-baseline space-x-4">
                   <NavLink href="/">
                     <a className="nav">Home</a>
