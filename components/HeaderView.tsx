8d288e0ec176e7fb052f1a40d75cb31369f33000
Karl <karl@kejk.tech>
2023-07-16 21:56:48 +0100

    Update About page subtitle rendering

--- components/HeaderView.tsx
+++ components/HeaderView.tsx
@@ -4,7 +4,7 @@ export default function HeaderView({ children }: { children: React.ReactNode }) 
   return (
     <header className='mx-auto w-full'>
       <div className='flex w-full justify-start px-4 pt-12 sm:px-6 md:justify-center lg:px-8'>
-        <h1 className='text-left text-3xl font-bold text-neutral-900 dark:text-neutral-200 md:text-center'>{children}</h1>
+        <h1 className='text-left scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl text-neutral-900 dark:text-neutral-200 md:text-center'>{children}</h1>
       </div>
     </header>
   );
